9ea01b5405e05c78ee3d21640df2a1cf52ee4cef
Stepan Bolshakov <stepan.bolshakov@gmail.com>
2015-11-26 21:11:52 +0300

    Mailgun setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,11 +82,11 @@ Rails.application.configure do
   }
 
   config.action_mailer.smtp_settings = {
-      :port => ENV['MAILGUN_SMTP_PORT']
-      :address => ENV['MAILGUN_SMTP_SERVER']
-      :user_name => ENV['MAILGUN_SMTP_LOGIN']
-      :password => ENV['MAILGUN_SMTP_PASSWORD']
-      :domain => 'young-stream-4064.herokuapp.com'
+      :port => ENV['MAILGUN_SMTP_PORT'],
+      :address => ENV['MAILGUN_SMTP_SERVER'],
+      :user_name => ENV['MAILGUN_SMTP_LOGIN'],
+      :password => ENV['MAILGUN_SMTP_PASSWORD'],
+      :domain => 'young-stream-4064.herokuapp.com',
       :authentication => :plain
   }
 
