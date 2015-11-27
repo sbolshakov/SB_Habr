3defdca962775b9f8816e39881951c2c355e113d
Stepan Bolshakov <stepan.bolshakov@gmail.com>
2015-11-27 23:20:10 +0300

    FA

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def link_with_icon(icon, path, options = {})
     title = options.delete(:title)
     link_to path, options do
-      content_tag(:i, title, class: "fa fa-#{icon}")
+      content_tag(:i, title, class: "fa fa-#{icon} h1")
     end
   end
 end
